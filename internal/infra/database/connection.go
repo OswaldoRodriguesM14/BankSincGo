679614d4d225286642693437492613319b2ba421
OswaldoRodriguesM14 <71694236+OswaldoRodriguesM14@users.noreply.github.com>
2024-04-20 11:41:19 -0300

    Implementacoes das Consultas e Lancamentos

--- internal/infra/database/connection.go
+++ internal/infra/database/connection.go
@@ -4,25 +4,27 @@ import (
 	"context"
 	"fmt"
 
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func ConnectDB() (*mongo.Client, error) {
-	// Configure options for the MongoDB client
-	opts := options.Client().ApplyURI("mongodb://localhost:27017")
+	// Use the SetServerAPIOptions() method to set the version of the Stable API on the client
+	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
+	opts := options.Client().ApplyURI("mongodb+srv://souzacla74:CyMpf9F5u67oyodh@cluster0.cscj27o.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0").SetServerAPIOptions(serverAPI)
 
-	// Create a new client and connect to the local MongoDB server
-	client, err := mongo.Connect(context.Background(), opts)
+	// Create a new client and connect to the server
+	client, err := mongo.Connect(context.TODO(), opts)
 	if err != nil {
 		return nil, err
 	}
 
 	// Send a ping to confirm a successful connection
-	if err := client.Ping(context.Background(), nil); err != nil {
+	if err := client.Database("admin").RunCommand(context.TODO(), bson.D{{"ping", 1}}).Err(); err != nil {
 		return nil, err
 	}
-
-	fmt.Println("Connected to MongoDB!")
+	fmt.Println("Pinged your deployment. You successfully connected to MongoDB!")
 	return client, nil
+
 }
